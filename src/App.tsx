894480d909096752124be8add037a3e2daa67db3
Pradip Singh <pradip.singh@varicon.com.au>
2024-08-22 08:54:37 +0545

    config App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Button from "@mui/material/Button";
 
 export default function ButtonUsage() {
-  return <Button variant="contained">Hello world</Button>;
+  return <Button variant="contained">Hello Varicon</Button>;
 }
