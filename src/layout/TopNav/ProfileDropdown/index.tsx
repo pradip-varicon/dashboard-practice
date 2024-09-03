f9ddb2ac49755aeabffd0a6d6c0558a2d7aeae69
Pradip Singh <pradip.singh@varicon.com.au>
2024-09-03 11:56:47 +0545

    adding dropdown to header

---
+++ src/layout/TopNav/ProfileDropdown/index.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Typography, Divider, MenuItem } from "@mui/material";
+import { useAuth } from "../../../context/AuthContext";
+import { Edit as EditIcon, ExitToApp as LogoutIcon } from "@mui/icons-material";
+import {
+  StyledMenu,
+  DisabledMenuItem,
+  MenuItemText,
+} from "./ProfileDropdownStyles";
+
+interface ProfileDropdownProps {
+  anchorEl: HTMLElement | null;
+  open: boolean;
+  onClose: () => void;
+}
+
+const ProfileDropdown: React.FC<ProfileDropdownProps> = ({
+  anchorEl,
+  open,
+  onClose,
+}) => {
+  const { user, logout } = useAuth();
+
+  const handleLogout = async () => {
+    await logout();
+    onClose();
+  };
+
+  return (
+    <StyledMenu
+      anchorEl={anchorEl}
+      open={open}
+      onClose={onClose}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+    >
+      <DisabledMenuItem>
+        <Typography variant="subtitle1">
+          {user?.firstName} {user?.lastName}
+        </Typography>
+      </DisabledMenuItem>
+      <DisabledMenuItem>
+        <Typography variant="subtitle2">{user?.email}</Typography>
+      </DisabledMenuItem>
+      <Divider />
+      <MenuItem>
+        <EditIcon fontSize="small" />
+        <MenuItemText variant="body2">Edit Profile</MenuItemText>
+      </MenuItem>
+      <Divider />
+      <MenuItem onClick={handleLogout}>
+        <LogoutIcon fontSize="small" />
+        <MenuItemText variant="body2">Logout</MenuItemText>
+      </MenuItem>
+    </StyledMenu>
+  );
+};
+
+export default ProfileDropdown;
